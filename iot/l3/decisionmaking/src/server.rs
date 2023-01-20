b77e9835ec3c1ca733b05bd9425699b5043a968e
kobala <bartlomiej.koba@gmail.com>
2023-01-20 12:15:12 +0100

    update from laptop

--- iot/l3/decisionmaking/src/server.rs
+++ iot/l3/decisionmaking/src/server.rs
@@ -1,40 +1,27 @@
-use std::{thread, time, time::Duration};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 
-fn eq(prev: f32, is_on: bool) -> f32{
-    let temp_delta: f32 = 0.1;
-    if is_on {
-        // temp goes up!
-        let val = prev + prev * temp_delta; 
-        return val;
-    }
-    else{
-        let val = prev + prev * (0.1);
-        return val;
-    }
-
-}
-
-fn simulation() -> i8 {
-    let eq = 2;
-    eq
-}
 
 fn main() {
-    let start = 23.0;
-    let outside = 0; // temperatura na zewnatrz
-    let current: f32; // temperatura pokoju
-    let mut index = 0;
-    let mut val: f32;
-    val = eq(start, true);
-    while index < 10{
-        val = eq(val, true);
-        println!("Iter: {index}, val: {}", val);
-        index += 1;
-        thread::sleep(Duration::from_secs(5))
-    }
+    let (tx, rx) = mpsc::channel();
 
-    let sample = eq(1000.0, true);
-    println!("Temp: {}", sample);
-    simulation();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
