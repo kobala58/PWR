2a678ae013061699854b9a8137b97e7ff8388b23
Kobala <bartlomiej.koba@gmail.com>
2023-03-06 23:27:15 +0100

    rewrite circle placements to polar coords system

--- projektowanie_algorytmow/l1.py
+++ projektowanie_algorytmow/l1.py
@@ -1,12 +1,10 @@
 from enum import Flag
-from math import sqrt
+from math import sin, sqrt, cos
+import math
 from typing import List
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-
-import matplotlib
-matplotlib.use('tkagg') # known bug, need to switch rendering engine
 
 def generate_nodes_from_input() -> tuple[nx.Graph, int]:
     nodes_count = int(input("Podaj ilosc wierzcholkow: "))
@@ -15,13 +13,28 @@ def generate_nodes_from_input() -> tuple[nx.Graph, int]:
     return (G, nodes_count)
 
 def ex_1():
+    RADIUS = 10
     G, nodes_count = generate_nodes_from_input()
     G.add_edges_from([(x,y) for x in range(nodes_count) for y in range(nodes_count) if x != y])
-    
-    pos = nx.circular_layout(G) # generate circular layout
-    
+    # pos = nx.circular_layout(G) simple solution provided by Networkx libary
+    # we need to implement own solution based on polar coordinate system
+    __space = 360/nodes_count # separtaor 
+    # pos = {x:[(RADIUS*cos(__space*x)).__round__(2), (RADIUS*sin(__space*x)).__round__(2)] for x in range(nodes_count)}
+    pos = {}
+    for x in range(nodes_count):
+        print(__space * x)
+        angle = (__space * x)*math.pi/180 
+        pos[x] = [(RADIUS*cos(angle)).__round__(2), (RADIUS*sin(angle)).__round__(2)] 
+
+    fig, ax = plt.subplots() #create canvas
+    print(pos)
+    nx.draw(G, pos=pos)
     nx.draw_networkx_labels(G, pos)
-    nx.draw_circular(G, node_size = 200)
+    plt.axis("on")
+    ax.add_patch(plt.Circle((0,0), RADIUS, fill=False, color="green"))
+    ax.set_xlim(-12,12)
+    ax.set_ylim(-12,12) # temp solution 
+    ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
     plt.show()
 
 def ex_2():
@@ -29,9 +42,8 @@ def ex_2():
     RANGSIZE_UP = 100
     G, nodes_count = generate_nodes_from_input() # grab graph from user input
     fig, ax = plt.subplots() #create canvas
-    pos = {x:[random.randint(RANGSIZE_DOWN,RANGSIZE_UP), random.randint(RANGSIZE_DOWN,RANGSIZE_UP)] for x in range(nodes_count)} # position saved into dictionary
-    
-    nx.draw(G, pos=pos, ax=ax) # drawing lines below
+    pos = {x:[random.randint(RANGSIZE_DOWN,RANGSIZE_UP), random.randint(RANGSIZE_DOWN,RANGSIZE_UP)] for x in range(nodes_count)} # position in dictionary
+    nx.draw(G, pos=pos, ax=ax)
     nx.draw_networkx_labels(G, pos=pos)
     plt.axis("on")
     ax.set_xlim(RANGSIZE_DOWN-0.1*RANGSIZE_DOWN, RANGSIZE_UP+0.1*RANGSIZE_UP)
@@ -42,7 +54,7 @@ def ex_2():
 def ex_3():
     RANGSIZE_DOWN = 0
     RANGSIZE_UP = 100
-    RADIUS = 10
+    RADIUS = 5
     G, nodes_count = generate_nodes_from_input()
     G = nx.Graph() # non optimal solution but i dont want to repeat code
 
@@ -85,6 +97,6 @@ def ex_3():
     plt.show()
 
 if __name__ == "__main__":
-    # ex_1()
+    ex_1()
     # ex_2()
-    ex_3()
+    # ex_3()
