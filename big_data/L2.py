1141aea6cff512f426c4e597acec399bf2676a57
kobala <bartlomiej.koba@gmail.com>
2023-03-20 20:20:04 +0100

    daily update

--- big_data/L2.py
+++ big_data/L2.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures, SplineTransformer
 from sklearn.pipeline import make_pipeline
-
+from numpy.polynomial.polynomial import Polynomial
 from scipy import interpolate
 
-
+import matplotlib
+matplotlib.use('tkagg') # used on my laptop, pip packages seems broken there
 def data_gen():
 
     return 0 
@@ -15,7 +16,7 @@ def data_gen():
 def ex1():
     x = np.linspace(0, 4, 15)
     y = np.cos(x**2/3-1)
-
+    
     f1 = interpolate.interp1d(x, y, kind = 'linear')
     print(f1)
 
@@ -23,14 +24,13 @@ def ex1():
 
     plt.plot(x, y, 'o', xnew, f1(xnew), '-')
 
+    plt.legend()
     plt.show()
 
 
 def ex2():
-    # ex2 is kinda similar to ex1 if we decided to stick to scipy
-    # it is too simple so i decided to try using 
     # const setup
-    SAMPLE_SIZE = 8
+    SAMPLE_SIZE = 4
     ALPHA = 0.01
     # basic data for plot generation
     x =  np.linspace(0,4,50)
@@ -39,19 +39,19 @@ def ex2():
     ax.plot(x, y)
     # generate random points to from range to
     x_train = np.linspace(0,4,50)
-    rng = np.random.RandomState(0)
-    x_train = np.sort(rng.choice(x_train, size=SAMPLE_SIZE, replace=False)) # this makes that we select random points from range
+    rng = np.random.RandomState(1)
+    # x_train = np.sort(rng.choice(x_train, size=SAMPLE_SIZE, replace=False)) # this makes that we select random points from range
     # generate val from random prev generated random points
-    y_train = np.cos(x_train**2/3-1)
-    # convert them to 2D arrays
-    X_train = x_train[:, np.newaxis]
-    X_plot = x[:, np.newaxis]
-    ax.scatter(x_train, y_train, label="training points")
+    # convert them to 2D array
 
-    model = make_pipeline(PolynomialFeatures(3), Ridge(alpha=ALPHA))
-    model.fit(X_train, y_train)
-    y_plot = model.predict(X_plot)
-    ax.plot(x, y_plot)
+    x_train = np.array([0, 2.5, 3.5, 4])
+    y_train = np.cos(x_train**2/3-1)
+    ax.scatter(x_train, y_train, label="training points")
+    print(x_train)
+    poly = interpolate.lagrange(x_train, y_train)
+    print(poly.coef)
+    ax.plot(x, Polynomial(poly.coef[::-1])(x), label="Predicted interpol")
+    plt.legend()
     plt.show()
 
 
@@ -60,7 +60,6 @@ def ex3():
     # TODO: Write comment about intention and usage
     # const setup
     SAMPLE_SIZE = 8
-    ALPHA = 0.01
     # basic data for plot generation
     x =  np.linspace(0,4,50)
     y = np.cos(x**2/3-1) # this is basic funciont that we will try to approx
@@ -81,10 +80,12 @@ def ex3():
     model.fit(X_train, y_train)
     y_plot = model.predict(X_plot)
 
-    ax.plot(x, y_plot)
+    ax.plot(x, y_plot, color="brown", label="predicted function")
+    plt.legend()
     plt.show() 
 
 if __name__ == "__main__":
+    ex1()
+    ex2()
     ex3()
-
     # TODO: Write summary
--- projektowanie_algorytmow/l3.py
+++ projektowanie_algorytmow/l3.py
@@ -1,6 +1,18 @@
-
+from enum import Enum
 from dataclasses import dataclass
 from typing import Dict
+
+# TODO:
+# detect first position 
+# transfrom letter into another letter
+# move left and right
+# 
+
+class Moves(Enum):
+    LEFT = "l"
+    RIGHT = "r"
+    TRANSFORM = "t"
+    NONE = "r"
 
 @dataclass
 class Language():
@@ -81,16 +93,59 @@ class Machine():
             return [False, "Lang is not provided"]
         
         next_node = self.start_node
-        for letter in word:
-            # grab start node
-            nn = next_node
-            next_node = self.nodes[next_node][letter]
-            print(f"Moved from {nn} to {next_node} by letter {letter}")
-        print("\n\n---Result---")
+        # for letter in word:
+        #     # grab start node
+        #     nn = next_node
+        #     # move to next node
+        #     next_node = self.nodes[next_node][letter]
+        #     print(f"Moved from {nn} to {next_node} by letter {letter}")
+        
+        cnt = 0 
+        word = [x for x in word]
 
+        while (cnt != -1) or (cnt == len(word)):
+            nn = next_node
+            tmp = self.nodes[next_node][word[cnt]]["to"]
+            # if rename exists 
+            if self.nodes[next_node][word[cnt]]["rename"] is not None:
+                word[cnt]=self.nodes[next_node][word[cnt]]["rename"]
+            
+            # now cnt manipulation
+
+            match self.nodes[next_node][word[cnt]]["direction"]:
+                case "left":
+                    cnt -= 1
+                case "right":
+                    cnt += 1
+
+            
+        # results
+        print("\n\n---Result---")
         if next_node in self.end_node:
             print(f"Word {word} is valid")
         else:
             print(f"Word {word} is invalid")
         print("\n\n")
 
+
+def ex1():
+    rho2 = {
+            "q0": {"a": {"to": "q1",
+                         "rename": "a_dot",
+                         "direction": "left"
+                         }}
+            }
+
+    ex1 = Machine(
+            start_node="q0",
+            end_node=["q3"],
+            lang=Language(("0","1")),
+            nodes=rho
+            )
+    # ex1.run_test("1100101011")
+    # ex1.run_test("11100")
+    ex1.run_test(input("Enter word in lang: "))
+
+
+if __name__ == "__main__":
+    ex1()
