e1a65c895742e6a2d8598943af396506facc7b89
Kobala <bartlomiej.koba@gmail.com>
2023-04-25 08:31:02 +0200

    tweaks

---
+++ big_data/l6.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def k_closest():
+    pass
+
+def svm():
+    pass
+
+def dectree():
+    pass
+
+
+def ex1():
+    pass
+
+def run_class(data: ):
+    pass
+
+if __name__ == "__main__":
+    ex1()
